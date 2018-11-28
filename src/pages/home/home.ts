f65f94504a7ae0eb89e64e8ff24030e988a57c3b
caroljunq <junq.carolina@gmail.com>
2018-11-28 20:32:26 -0200

    buscando 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,9 @@ import { Component, ElementRef, ViewChild, NgZone } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation';
 import { GoogleMapsProvider } from '../../providers/google-maps/google-maps';
 
+
+declare var google;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -22,13 +25,21 @@ export class HomePage {
       searchDisabled: boolean;
       saveDisabled: boolean;
       location: any;
+      directionsService: any;
+      directionsDisplay: any;
+      map: any;
+      startPosition: any;
+      originPosition: string;
+      destinationPosition: string;
 
       constructor(public navCtrl: NavController, public zone: NgZone, public maps: GoogleMapsProvider, public platform: Platform, public geolocation: Geolocation, public viewCtrl: ViewController) {
           this.searchDisabled = true;
           this.saveDisabled = true;
       }
 
-      ionViewDidLoad(): void {
+      ionViewDidLoad(){
+
+        // this.showCurrentLocation();
 
           let mapLoaded = this.maps.init(this.mapElement.nativeElement, this.pleaseConnect.nativeElement).then(() => {
 
@@ -36,9 +47,37 @@ export class HomePage {
               this.placesService = new google.maps.places.PlacesService(this.maps.map);
               this.searchDisabled = false;
 
+              this.directionsService = new google.maps.DirectionsService();
+              this.directionsDisplay = new google.maps.DirectionsRenderer();
+
           });
 
+
+
       }
+//
+//       showCurrentLocation(){
+//   this.geolocation.getCurrentPosition()
+//     .then((resp) => {
+//       const position = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+//
+//       const mapOptions = {
+//         zoom: 17,
+//         center: position
+//       }
+//
+//       this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+//
+//       const marker = new google.maps.Marker({
+//         position: position,
+//         map: this.map
+//       });
+//
+//     }).catch((error) => {
+//       console.log('Erro ao recuperar sua posição', error);
+//     });
+// }
+
 
       selectPlace(place){
 
@@ -106,5 +145,44 @@ export class HomePage {
       close(){
           this.viewCtrl.dismiss();
       }
+
+      // initializeMap() {
+//   this.startPosition = new google.maps.LatLng(-21.763409, -43.349034);
+//
+//   const mapOptions = {
+//     zoom: 18,
+//     center: this.startPosition,
+//     disableDefaultUI: true
+//   }
+//
+//   this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+//   this.directionsDisplay.setMap(this.map);
+//
+//   const marker = new google.maps.Marker({
+//     position: this.startPosition,
+//     map: this.map,
+//   });
+// }
+
+calculateRoute() {
+  if (this.destinationPosition && this.originPosition) {
+    const request = {
+      // Pode ser uma coordenada (LatLng), uma string ou um lugar
+      origin: this.originPosition,
+      destination: this.destinationPosition,
+      travelMode: 'WALKING'
+    };
+
+    this.traceRoute(this.directionsService, this.directionsDisplay, request);
+  }
+}
+
+traceRoute(service: any, display: any, request: any) {
+  service.route(request, function (result, status) {
+    if (status == 'OK') {
+      display.setDirections(result);
+    }
+  });
+}
 
   }
